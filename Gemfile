5fe77d4156bae72969bed8b19bf6c927734c4fd1
Gilbert Townshend <gilbert.townshend@gmail.com>
2024-03-28 19:18:39 +0000

    simple thumbnailer is broken

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "2.7.2"
+ruby "3.2.3"
 
 gem "bourbon"
 gem "builder"
--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 activate :autoprefixer
 activate :inline_svg
 activate :directory_indexes
-activate :middleman_simple_thumbnailer
+# activate :middleman_simple_thumbnailer
 
 # Generate a page for each image listed in the data file
 @app.data.portfolio.image_list.each_with_index do |image, current|
